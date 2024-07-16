c3de0d8079df3031aaa80a2e55023fec2263dac1
Lucas Lessa Gomes <lucaslessagomes@gmail.com>
2024-07-16 14:44:48 -0300

    Finalizado o projeto, adicionado a tela de 404 error

--- src/componentes/PostModelo/index.js
+++ src/componentes/PostModelo/index.js
@@ -1,4 +1,3 @@
-import { Children } from 'react'
 import styles from './PostModelo.module.css'
 
 const PostModelo = ({ fotoCapa, titulo, children}) => {
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import SobreMim from "./paginas/sobreMim";
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape/Index";
 import PaginaPadrao from "componentes/PaginaPadrao";
+import Post from "paginas/Post";
+import NaoEncontrada from "paginas/NaoEncontrada";
 
 function AppRoutes() {
   return (
@@ -16,7 +18,8 @@ function AppRoutes() {
           <Route path="sobremim" element={<SobreMim />}/>
         </Route>
 
-        <Route path="*" element={<div>Página não encontrada</div>} />
+        <Route path="posts/:id" element={<Post />} />
+        <Route path="*" element={<NaoEncontrada/>} />
       </Routes>
 
       <Rodape />
